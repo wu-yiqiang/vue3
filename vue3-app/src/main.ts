19b998c22c6069f6db57f5edd61cc92f4cc96057
wu-yiqiang <wu_yiqiang@outlook.com>
2023-07-09 17:13:17 +0800

    sad

--- vue3-app/src/main.ts
+++ vue3-app/src/main.ts
@@ -4,7 +4,6 @@ import 'virtual:svg-icons-register'
 import { useI18n } from '/@/plugins/i18n'
 import { setupStore } from '/@/store'
 import { setupVant } from '/@/plugins/vant'
-// import './assets/css/index.scss'
 import router from '/@/router'
 import App from '/@/App.vue'
 const app = createApp(App)
